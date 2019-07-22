6ba4017dfb854cc433601f408eb19c27a37ff634
berry120 <berry120@gmail.com>
2019-07-22 23:06:04 +0100

    Add PrimeRule

---
+++ src/main/java/org/berry/fizzbuzz/PrimeRule.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.berry.fizzbuzz;
+
+import java.math.BigInteger;
+import java.util.Optional;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+/**
+ *
+ * @author Michael
+ */
+@EqualsAndHashCode
+public class PrimeRule implements Rule {
+
+    @Getter
+    private final String substitute;
+
+    private PrimeRule(String substitute) {
+        this.substitute = substitute;
+    }
+
+    public static Rule of(String substitute) {
+        return new PrimeRule(substitute);
+    }
+
+    @Override
+    public boolean matches(int n) {
+        return new BigInteger(Integer.toString(n)).isProbablePrime(100);
+    }
+
+    @Override
+    public String substitute(int n) {
+        if (matches(n)) {
+            return substitute;
+        } else {
+            return Integer.toString(n);
+        }
+    }
+
+    @Override
+    public String getHumanDescription() {
+        return "Substitute \'" + substitute + "\' if prime";
+    }
+    
+    @Override
+    public boolean alreadyCoveredBy(Rule rule) {
+        return Optional.of(rule)
+                .filter(r -> r instanceof PrimeRule)
+                .isPresent();
+    }
+
+}
